b29083e7d751afeb3838e95148c76d07e930edba
aisyakareninaa <ninakudojaya@gmail.com>
2022-04-13 23:32:48 +0700

    first commit

---
+++ src/Latihan1/ujibuss.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan1;
+
+/**
+ *
+ * @author Panda PC
+ */
+public class ujibuss {
+     public static void main(String[] args) {
+        // TODO code application logic here
+        bus busMini=new bus();
+        busMini.penumpang=5;
+        busMini.maxpenumpang=5;
+        busMini.cetak();
+        
+        busMini.penumpang=busMini.penumpang+5;
+        busMini.cetak();
+        
+        busMini.penumpang=busMini.penumpang-2;
+        busMini.cetak();
+        
+        
+        busMini.penumpang=busMini.penumpang+8;
+        busMini.cetak();
+        
+          
+    }
+
+}
---
+++ src/TugasPraktikum/OutputBola.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TugasPraktikum;
+
+/**
+ *
+ * @author Panda PC
+ */
+public class OutputBola {
+    public static void main(String[] args) {
+        Bola hitung = new Bola();
+        hitung.setJari(9);
+        hitung.showD();
+        hitung.showLP();
+        hitung.showV();
+    }
+}
+
